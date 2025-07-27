254644b80723e8ce9bad8957ab8fa21dca891905
Vijay Menon <bakamenon@gmail.com>
2025-07-27 18:04:59 +0900

    WIP00002 - Clean up formatting as I have OCD

--- Adafruit_SH1106.h
+++ Adafruit_SH1106.h
@@ -32,14 +32,14 @@ typedef volatile uint8_t PortReg;
 typedef uint8_t PortMask;
 #endif
 
-#include <SPI.h>
 #include <Adafruit_GFX.h>
+#include <SPI.h>
 
 #define BLACK 0
 #define WHITE 1
 #define INVERSE 2
 
-#define SH1106_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x3C or 0x3D
+#define SH1106_I2C_ADDRESS 0x3C // 011110+SA0+RW - 0x3C or 0x3D
 // Address for 128x32 is 0x3C
 // Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
 
@@ -71,18 +71,18 @@ typedef uint8_t PortMask;
 #endif
 
 #if defined SH1106_128_64
-#define SH1106_LCDWIDTH                  128
-#define SH1106_LCDHEIGHT                 64
+#define SH1106_LCDWIDTH 128
+#define SH1106_LCDHEIGHT 64
 #endif
 
 #if defined SH1106_128_32
-#define SH1106_LCDWIDTH                  128
-#define SH1106_LCDHEIGHT                 32
+#define SH1106_LCDWIDTH 128
+#define SH1106_LCDHEIGHT 32
 #endif
 
 #if defined SH1106_96_16
-#define SH1106_LCDWIDTH                  96
-#define SH1106_LCDHEIGHT                 16
+#define SH1106_LCDWIDTH 96
+#define SH1106_LCDHEIGHT 16
 #endif
 
 #define SH1106_SETCONTRAST 0x81
@@ -103,14 +103,14 @@ typedef uint8_t PortMask;
 
 #define SH1106_SETMULTIPLEX 0xA8
 
-#define SH1106_SETLOWCOLUMN 0x02 //to use with SSD1306, set to 0x00
+#define SH1106_SETLOWCOLUMN 0x02 // to use with SSD1306, set to 0x00
 #define SH1106_SETHIGHCOLUMN 0x10
 
 #define SH1106_SETSTARTLINE 0x40
 
 #define SH1106_MEMORYMODE 0x20
 #define SH1106_COLUMNADDR 0x21
-#define SH1106_PAGEADDR   0x22
+#define SH1106_PAGEADDR 0x22
 
 #define SH1106_COMSCANINC 0xC0
 #define SH1106_COMSCANDEC 0xC8
@@ -135,49 +135,55 @@ typedef uint8_t PortMask;
 
 class Adafruit_SH1106 : public Adafruit_GFX {
 public:
-Adafruit_SH1106(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
-Adafruit_SH1106(int8_t DC, int8_t RST, int8_t CS);
+  Adafruit_SH1106(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
+  Adafruit_SH1106(int8_t DC, int8_t RST, int8_t CS);
 
-Adafruit_SH1106(uint8_t RST);
-Adafruit_SH1106(int8_t SDA=-1, int8_t SCL=-1);
+  Adafruit_SH1106(uint8_t RST);
+  Adafruit_SH1106(int8_t SDA = -1, int8_t SCL = -1);
 
-void begin(uint8_t switchvcc = SH1106_SWITCHCAPVCC, uint8_t i2caddr = SH1106_I2C_ADDRESS, bool reset=true);
-void sh1106_command(uint8_t c);
-void sh1106_data(uint8_t c);
+  void begin(uint8_t switchvcc = SH1106_SWITCHCAPVCC,
+             uint8_t i2caddr = SH1106_I2C_ADDRESS, bool reset = true);
+  void sh1106_command(uint8_t c);
+  void sh1106_data(uint8_t c);
 
-void clearDisplay(void);
-void invertDisplay(uint8_t i);
-void display();
+  void clearDisplay(void);
+  void invertDisplay(uint8_t i);
+  void display();
 
-// Implement scrolll using software - the hacky way. Need to add variables in to the class public area
-void startscrollright(uint8_t start, uint8_t stop);
-// void startscrollleft(uint8_t start, uint8_t stop, uint8_t cursor_x, uint8_t cursor_y);
-// void startscrolldiagright(uint8_t start, uint8_t stop, uint8_t cursor_x, uint8_t cursor_y);
-// void startscrolldiagleft(uint8_t start, uint8_t stop, uint8_t cursor_x, uint8_t cursor_y);
-// void stopscroll(uint8_t cursor_x, uint8_t cursor_y);
+  // Implement scrolll using software - the hacky way. Need to add variables in
+  // to the class public area
+  void startscrollright(uint8_t start, uint8_t stop);
+  // void startscrollleft(uint8_t start, uint8_t stop, uint8_t cursor_x, uint8_t
+  // cursor_y); void startscrolldiagright(uint8_t start, uint8_t stop, uint8_t
+  // cursor_x, uint8_t cursor_y); void startscrolldiagleft(uint8_t start,
+  // uint8_t stop, uint8_t cursor_x, uint8_t cursor_y); void stopscroll(uint8_t
+  // cursor_x, uint8_t cursor_y);
 
-void dim(boolean dim);
+  void dim(boolean dim);
 
-void drawPixel(int16_t x, int16_t y, uint16_t color);
+  void drawPixel(int16_t x, int16_t y, uint16_t color);
 
-virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
 
 private:
-int8_t _i2caddr, _vccstate, sid, sclk, dc, cs, sda, scl;
-uint8_t rst;
-void fastSPIwrite(uint8_t c);
+  int8_t _i2caddr, _vccstate, sid, sclk, dc, cs, sda, scl;
+  uint8_t rst;
+  void fastSPIwrite(uint8_t c);
 
-boolean hwSPI;
+  boolean hwSPI;
 #ifndef ESP32
-PortReg *mosiport, *clkport, *csport, *dcport;
-PortMask mosipinmask, clkpinmask, cspinmask, dcpinmask;
+  PortReg *mosiport, *clkport, *csport, *dcport;
+  PortMask mosipinmask, clkpinmask, cspinmask, dcpinmask;
 #else
-volatile uint32_t *mosiport, *clkport, *csport, *dcport;
-volatile uint32_t mosipinmask, clkpinmask, cspinmask, dcpinmask;
+  volatile uint32_t *mosiport, *clkport, *csport, *dcport;
+  volatile uint32_t mosipinmask, clkpinmask, cspinmask, dcpinmask;
 #endif
 
-inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
-inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
-
+  inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h,
+                                    uint16_t color)
+      __attribute__((always_inline));
+  inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w,
+                                    uint16_t color)
+      __attribute__((always_inline));
 };
